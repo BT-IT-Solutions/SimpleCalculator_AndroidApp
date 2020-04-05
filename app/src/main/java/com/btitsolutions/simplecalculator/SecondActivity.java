b0f544e1a85fa4c6759f1f23d469928c3dc19376
C_Bereket Tolcha <BT11@security-finance.com>
2020-04-05 16:33:52 -0400

    - second activity file added for java and layout

---
+++ app/src/main/java/com/btitsolutions/simplecalculator/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.btitsolutions.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
